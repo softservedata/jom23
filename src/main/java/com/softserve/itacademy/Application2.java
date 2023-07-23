00e6be38c78d42e0db883742c807f0b78588cdb4
Yaroslav <garasym@gmail.com>
2023-07-23 21:49:18 +0300

    add Application2

---
+++ src/main/java/com/softserve/itacademy/Application2.java
@@ -0,0 +1,39 @@
+package com.softserve.itacademy;
+
+import com.softserve.itacademy.model.ToDo;
+import com.softserve.itacademy.model.User;
+import com.softserve.itacademy.service.ToDoService;
+import com.softserve.itacademy.service.UserService;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+
+public class Application {
+
+    public static void main(String[] args) {
+       AnnotationConfigApplicationContext annotationConfigContext = new AnnotationConfigApplicationContext(Config.class);
+        UserService userService = annotationConfigContext.getBean(UserService.class);
+        ToDoService toDoService = annotationConfigContext.getBean(ToDoService.class);
+        User user = new User("Mike", "Pears", "coc@ga", "12345", new ArrayList<>());
+        ToDo toDo = new ToDo("Chill on the beach", LocalDateTime.now(), user, new ArrayList<>());
+
+        userService.addUser(user);
+        toDoService.addTodo(toDo, user);
+
+        System.out.println(userService.getAll().size());
+        System.out.println(toDoService.getAll().size());
+        toDoService.deleteTodo(toDo);
+        System.out.println(userService.getAll().size());
+        System.out.println(toDoService.getAll().size());
+
+
+
+
+
+        annotationConfigContext.close();
+
+
+    }
+
+}
